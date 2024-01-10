056e5b059d87a0b6dba0eb0e3dca947e28ce398f
jason-p-pickering <jason.p.pickering@gmail.com>
2024-01-10 09:03:22 +0100

    Linting

--- src/components/BackupWorkflow.js
+++ src/components/BackupWorkflow.js
@@ -3,6 +3,6 @@ export class BackupWorkflow extends HTMLElement {
         this.innerHTML = `<h2 class="backup-config" data-i18n="backup-config.title"></h2>
     <p class="backup-config" data-i18n="backup-config.content"></p>
     <button data-i18n="backup-config.backup-btn" onclick="exportLocalIndicators()" id="btn-backup-local-config"></button>`;
-}
+    }
 }
 
